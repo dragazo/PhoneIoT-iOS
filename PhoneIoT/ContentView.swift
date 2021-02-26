fffd711159b8a46300674520bafb0c7f5c612069
Devin Jean <emailcruzjean@yahoo.com>
2021-02-25 22:56:18 -0600

    Initial Commit

---
+++ PhoneIoT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhoneIoT
+//
+//  Created by Devin Jean on 2/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhoneIoT/PhoneIoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneIoTApp.swift
+//  PhoneIoT
+//
+//  Created by Devin Jean on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct PhoneIoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
