8f8274779a1f5189e7ecb2d52e7000d507d3cde7
Devin Jean <emailcruzjean@yahoo.com>
2023-01-24 21:15:47 -0600

    empty watch app

---
+++ WatchIoT/WatchIoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchIoTApp.swift
+//  WatchIoT
+//
+//  Created by Devin Jean on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchIoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
